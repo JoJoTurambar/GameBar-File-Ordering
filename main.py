3c469ba97b96e73c918122f1838a1f10d22f7044
JoJoTurambar <jesus.escanilla@usm.cl>
2025-10-26 23:45:49 -0300

    Solved format errors

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import os
-
-# date format
-DATE_FORMAT = "%d-%m-%Y"
+import re
 
 # unique games
 UNIQUE_GAMES = []
@@ -16,37 +14,21 @@ def load_files_in_directory(directory):
 
 def is_date_or_time(part):
     # Check for date format dd-mm-yyyy i.e. 07-02-2025
-    if len(part) == 10 and part[2] == '-' and part[5] == '-':
-        day, month, year = part.split('-')
-        if day.isdigit() and month.isdigit() and year.isdigit():
-            return True
+    re1 = re.compile(r'\d{2}-\d{2}-\d{4}')
+    if re1.fullmatch(part):
+        return True
         
-    # Check for time format hh_mm_ss i.e. 21_59_54
-    if len(part) == 8 and part[2] == '_' and part[5] == '_':
-        hour, minute, second = part.split('_')
-        if hour.isdigit() and minute.isdigit() and second.isdigit():
-            return True
-        
-    # Check for time format h_mm_ss i.e. 4_02_15
-    if len(part) == 7 and part[1] == '_' and part[4] == '_':
-        hour, minute, second = part.split('_')
-        if hour.isdigit() and minute.isdigit() and second.isdigit():
-            return True
+    # Check for date format dd-mm-yyyy i.e. 2025-02-07
+    re2 = re.compile(r'\d{4}-\d{2}-\d{2}')
+    if re2.match(part):
+        return True
+    
+    # Check for time format hh_mm_ss i.e. 21_59_54 4_02_15 4-02-15
+    re3 = re.compile(r'\d{1,2}[-_]\d{1,2}[-_]\d{1,2}')
+    if re3.match(part):
+        return True
         
     return False
-
-# Delete date, time and extension from filenames & register unique games
-def clean_filenames(filenames):
-    cleanFiles = []
-    for file in filenames:
-        name, _ = os.path.splitext(file)
-        parts = name.split(' ')
-        clean_name = ' '.join(part for part in parts if not is_date_or_time(part))
-        cleanFiles.append(clean_name)
-        if clean_name not in UNIQUE_GAMES:
-            UNIQUE_GAMES.append(clean_name)
-    
-    return cleanFiles
 
 def clean(filename):
     name, _ = os.path.splitext(filename)
